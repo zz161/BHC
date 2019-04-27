40aa198746120d9cba9b9c89ebb5cf4cc1f77761
Zhuangdie(Alan) Zhou <43016130+zz161@users.noreply.github.com>
2019-04-27 17:19:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
 setuptools.setup(
      name='BayesianClustering',  
      version='0.1',
-     author="Azucena Morales and Alan Zhou",
-     author_email="lm348@duke.edu and zz161@duke.edu",
+     author="Azucena Morales<lm348@duke.edu>, Alan Zhou<zz161@duke.edu>",
+     author_email="alanazu@googlegroups.com",
      description="Bayesian Hierachichal Clustering using Heller's article",
      url="https://github.com/AzucenaMV/STAT-663",
      packages=setuptools.find_packages(),
