dd5a97532b3d38b9a59696664145b73566b2a876
Azucena <azucena.mova@gmail.com>
2019-04-27 00:16:48 -0400

    Add setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='BayesianClustering',  
+     version='0.1',
+     scripts=['bhc','node'] ,
+     author=["Azucena Morales","Alan Zhou"],
+     author_email=["lm348@duke.edu","zz161@duke.edu"],
+     description="Bayesian Hierachichal Clustering using Heller's article",
+     url="https://github.com/AzucenaMV/STAT-663",
+     packages=setuptools.find_packages(['numpy','itertools','scipy','functools']),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
